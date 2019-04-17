1063d3a96be336d26c02141d70cb7794b896a161
kaix <kaixokkiten@gmail.com>
2019-04-17 16:56:53 -0400

    testing!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.println("Hello, World! I made a change. I made another change. push it.");
+		System.out.println("kaix has deleted everything.");
 	}
 }
